8b2627ccf6a566d2e075804f36514d9b9e66ddc1
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-05-23 18:25:52 +0530

    0.4.0 - modified newsItem & integrated state and constructor to News.js

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
 
 export default class NewsItem extends Component {
+ 
+
   render(props) {
-    let {title, description} = this.props;
+    let {title, description, imageUrl, newsUrl} = this.props;
     // we are destructuring and pulling out our title and description from the props input to the newsItem 
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-          <img src="https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/18446/production/_129789399_microsoftteams-image-4.png" className="card-img-top" alt="..." />
+        <div className="card my-3" style={{width: "18rem"}}>
+          <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">
               {description}
             </p>
-            <a href="/newsDetails" className="btn btn-primary">
+            <a href="/newsDetails" className="btn btn-sm btn-primary">
               Go somewhere
             </a>
           </div>
