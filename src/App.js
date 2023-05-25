d9faff91ba09fcecf0fcff5ac757953d2a2d476d
Harman Singh <98532264+H-SM@users.noreply.github.com>
2023-05-25 19:03:51 +0530

    0.8.0 - categories added & router link initialized

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import Navbar from './components/Navbar';
-import News from './components/News';
-import './App.css';
-
-import React, { Component } from 'react'
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { Component } from "react";
+// import About from "../../app-here/src/components/About";
 
 export default class App extends Component {
-
-  
   render() {
     return (
-      <div >
-        <Navbar/>
-        <News pageSize={10} country="in" category="health"/>
-      </div>
+      <Router>
+        <>
+          <div>
+            <Navbar />
+            <div className="container my-3">
+              <Routes>
+                <Route exact path="/" element={<News key="gen_home" pageSize={10} country="in" category="general"/>} />
+                <Route exact path="/business" element={<News key="business" pageSize={10} country="in" category="business"/>} />
+                <Route exact path="/entertainment" element={<News key="entertainment" pageSize={10} country="in" category="entertainment"/>} />
+                <Route exact path="/general" element={<News key="general" pageSize={10} country="in" category="general"/>} />
+                <Route exact path="/health" element={<News key="health" pageSize={10} country="in" category="health"/>} />
+                <Route exact path="/science" element={<News key="science" pageSize={10} country="in" category="science"/>} />
+                <Route exact path="/sports" element={<News key="sports" pageSize={10} country="in" category="sports"/>} />
+                <Route exact path="/technology" element={<News key="technology" pageSize={10} country="in" category="technology"/>} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      </Router>
       // style={{color: "white",backgroundColor: "#042743"}}
-    )
+    );
   }
 }
-
